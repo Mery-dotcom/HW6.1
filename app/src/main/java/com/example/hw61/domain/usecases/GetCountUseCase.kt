a947506fcbd8bb4da97447476f2bf5eccdd31f93
Mery-dotcom <meka-28list.ru>
2025-03-17 19:22:20 +0600

    add new branch

--- app/src/main/java/com/example/hw61/domain/usecases/GetCountUseCase.kt
+++ app/src/main/java/com/example/hw61/domain/usecases/GetCountUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.hw61.domain.usecases
 
-import com.example.hw61.domain.model.CounterEntity
 import com.example.hw61.domain.repository.CounterRepository
 
 class GetCountUseCase(
--- app/src/main/java/com/example/hw61/presentation/fragment/CounterFragment.kt
+++ app/src/main/java/com/example/hw61/presentation/fragment/CounterFragment.kt
@@ -1,6 +1,5 @@
 package com.example.hw61.presentation.fragment
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
