9d63194870ef6aae19c747af74cb22518482842c
Mery-dotcom <meka-28list.ru>
2025-03-19 19:07:20 +0600

    Dispatchers add

--- app/src/main/java/com/example/hw61/data/model/Either.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hw61.data.model
-
-sealed class Either<out L, out R> {
-
-    data class Success<R>(val success: R) : Either<Nothing, R>()
-    data class Error<L>(val error: L) : Either<L, Nothing>()
-}
--- app/src/main/java/com/example/hw61/presentation/view/viewmodels/ExchangeRateViewModel.kt
+++ app/src/main/java/com/example/hw61/presentation/view/viewmodels/ExchangeRateViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.hw61.data.model.Either
 import com.example.hw61.data.model.ExchangeRatesResponse
 import com.example.hw61.domain.usecases.GetExchangeRatesUseCase
 import kotlinx.coroutines.launch
